640718376e53dcb041504835f6a26ee2a185284d
mateomenestrina <mateo.menestrina@gmail.com>
2013-08-08 12:15:41 -0300

    6
    6

---
+++ js/index.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    
+    
+
+    // Application Constructor
+    initialize: function() {
+        
+        this.bindEvents();
+    },
+  
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
+
+    onDeviceReady: function() {
+
+        this.receivedEvent('deviceready');
+        
+    },
+    
+    receivedEvent: function(id) {
+        
+    
+    
+    },
+    
+    camera_getPicture: function(){
+        navigator.camera.getPicture( app.onSuccess, app.onFail, { quality: 100, sourceType:Camera.PictureSourceType.PHOTOLIBRARY,  destinationType: Camera.DestinationType.DATA_URL});
+
+    },
+
+    
+    onSuccess: function (imageData) {
+        var image = document.getElementById('myImageCamera');
+            image.src = "data:image/jpeg;base64," + imageData;
+            image.width = 100
+            image.height = 100
+    },
+    
+    onFail: function(message) {
+      alert('Failed because: ' + message);
+    }
+    
+    
+    
+};
