8ab253f43b7b8dd64a08c00cff4707a3a04cc6d6
mateomenestrina <mateo.menestrina@gmail.com>
2013-08-13 17:35:16 -0300

    sdfsdf
    sdf

--- js/index.js
+++ js/index.js
@@ -11,71 +11,68 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * "AS IS" BASIS, WITHOUT WARRANaTIES OR CONDaITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 var app = {
     
+    watchID : null,
     
-
-    // Application Constructor
     initialize: function() {
         
-        this.bindEvents();
-    },
-  
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    
-
-    onDeviceReady: function() {
-
-        this.receivedEvent('deviceready');
-  
-        setInterval(function(){
-             navigator.accelerometer.getCurrentAcceleration(app.onSuccess, app.onError);
-        }, 1000)
        
-
-    },
-    
-    onSuccess: function (acceleration) {
-        $('#acel').html('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n')
-    },
-
-   onError: function () {
-        alert('onError!');
-    },
-
-    receivedEvent: function(id) {
+        document.addEventListener('deviceready', app.onDeviceReady, false);
         
+    },
+    
+   
+    onDeviceReady: function() {
+        
+         app.startWatch();
+         
+    },
+                           
+
+    startWatch: function () {
+    
+        var options = { frequency: 3000 };
+        
+       // app.watchID =    navigator.accelerometer.watchAcceleration(app.onSuccess, app.onError, options);
+       
+       // $('#acel').html(app.watchID)
     
     
     },
-    
+
+    onSuccess: function (acceleration) {
+               
+         //alert(app.watchID)
+         
+    },
+
+    onError: function () {
+      alert('onError!');
+    },
+
     camera_getPicture: function(){
-        navigator.camera.getPicture( app.onSuccess, app.onFail, { quality: 100, sourceType:Camera.PictureSourceType.PHOTOLIBRARY,  destinationType: Camera.DestinationType.DATA_URL});
-
-    },
-
-    
-    onSuccess: function (imageData) {
-        var image = document.getElementById('myImageCamera');
-            image.src = "data:image/jpeg;base64," + imageData;
-            image.width = 100
-            image.height = 100
+         navigator.camera.getPicture( app.onSuccess, app.onFail, { quality: 100, sourceType:Camera.PictureSourceType.PHOTOLIBRARY,  destinationType: Camera.DestinationType.DATA_URL});
     },
     
-    onFail: function(message) {
-      alert('Failed because: ' + message);
-    }
     
+   onSuccess: function (imageData) {
+     var image = document.getElementById('myImageCamera');
+         image.src = "data:image/jpeg;base64," + imageData;
+         image.width = 100
+         image.height = 100
+   },
     
-    
-};
+   onFail: function(message) {
+       alert('Failed because: ' + message);
+   }
+
+}
+
+$(document).ready(app.initialize);
+
